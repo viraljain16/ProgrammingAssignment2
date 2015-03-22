755284a3313c16a12cfe3e9b732589ae964c5289
Viral Jain <viraljain@gmail.com>
2015-03-22 15:10:51 -0500

    updated code for matrix functions in assignment 2 for R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# create a caching vector function that accepts a matrix as its only argument and returns a list of caching functions.
 makeCacheMatrix <- function(x = matrix()) {
-
+		# initialize local variable 'inv' to NULL
+        inv <- NULL
+        		# check if the argument passed is a matrix or not. 
+        		if (!is.matrix(x))
+        		{
+        		  # if argument entered is not matrix then display message asking user to enter a matrix argument
+        		  print ("Enter Matrix Object as argument")
+        		  # return null and end execution here since argument entered is not matrix
+        		  return(NULL)
+        		}
+        # create a function called set that allows to set/accept the matrix value that the user passes to function and cache/store in 'x'. 
+        set <- function(y) {
+        		# check if the argument passed while trying to 'set' is a matrix or not. 
+        		if (!is.matrix(y))
+        		{
+        		  # if argument entered is not matrix then display message asking user to enter a matrix argument	
+        		  print ("Enter Matrix Object as argument")
+        		  # return null and end execution here since argument entered is not matrix
+        		  return(NULL)
+        		}
+          # assign the matrix that user entered to the object 'x'.
+          x <<- y
+          # reset the vector 'inv' as NULL since a new matrix has been set.
+          inv <<- NULL
+        }
+        # will print the cached matrix in 'x'.
+        get <- function() x
+        # create a function to assign an inverse matrix to the object 'inv'.
+        setinverse <- function(inverse) inv <<- inverse
+        # will print the cached inversed matrix
+        getinverse <- function() inv
+        # return (since its the last block of code) a list of the functions set, get, setinverse, getinverse 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
+# create a function to compute and return the inverse of the cached matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+		# read if there is already a cached inverse matrix in object x.
+        inv <- x$getinverse()
+        # if there is a cached inverse matrix in x then return the same.
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        # read the cached matrix from object x and assign to object data.
+        data <- x$get()
+        # compute the inverse of matrix stored in object 'data' and assign to object 'inv'.
+        inv <- solve(data, ...)
+        # set the inverse matrix 'inv' in to the object x.
+        x$setinverse(inv)
+        # return the object 'inv'
+        inv
 }
